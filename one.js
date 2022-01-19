b356e5e84942d66d3184e3ad56b8798fe676f088
Gabriel Seixas <salesseixasgabriel@gmail.com>
2022-01-19 20:24:05 -0300

    Create one.js

---
+++ one.js
@@ -0,0 +1,25 @@
+////// Primitivos
+// boolean = true
+// number = 2
+// string = "Hello"
+
+////// Não primitivos
+// array = [1, 2, 3]
+// object = {
+//   name: "John",
+//   age: 30
+// }
+// function = function() {}
+
+////// Operadores
+// + - * / %
+// == === != !== > < >= <=
+// && || !
+// = += -= *= /= %=
+
+var nomes = ["Gabriel", "Mario", "Lucas"];
+var idade = [18, 20, 22];
+
+//[{ nome: "Gabriel", idade: 18 }, { nome: "Mario", idade: 20 }, { nome: "Lucas", idade: 22 }]
+
+console.log()
